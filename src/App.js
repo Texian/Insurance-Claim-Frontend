dc814db0b546f546e5ccb57456365b8ca79d0b9b
cwalters <xian04@yahoo.com>
2020-05-11 22:11:00 -0700

    Sass installation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    <NavBar></NavBar>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
